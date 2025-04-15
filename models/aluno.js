058ae25880453bff55b1cd48629be9ff618541c4
Arthur1105 <olaalex682@gmail.com>
2025-04-15 20:52:08 -0300

    Revert"Tarefa 14/4"
    Correção de upload

--- models/aluno.js
+++ models/aluno.js
@@ -1,22 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
     const Aluno = sequelize.define("Aluno", {
-      nome: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      turma: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-      },
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
     });
-  
-    Aluno.associate = (models) => {
-      Aluno.belongsTo(models.Curso, {
-        foreignKey: "cursoId",
-        as: "Curso", 
-      });
-    };
-  
+
     return Aluno;
-  };
-  
+};
--- routes/alunos.js
+++ routes/alunos.js
@@ -1,112 +1,57 @@
 const express = require("express");
 const router = express.Router();
-const { Aluno, Curso } = require("../models"); // Ajuste o caminho conforme necessário
+const { Aluno } = require("../models");
 
-// Mostrar todos os alunos
-router.get("/",  async (req, res) => {
-  try {
-    const alunos = await Aluno.findAll({
-      include: [{ model: Curso, as: "Curso" }],
-    });
+// Listar alunos
+router.get("/", async (req, res) => {
+    const alunos = await Aluno.findAll();
     res.render("base", {
-      title: "Alunos", 
-      view: "alunos/show",
-      alunos,
+        title: "alunos",
+        view: "alunos/show",
+        alunos,
     });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Erro ao recuperar alunos");
-  }
 });
 
-// Formulário para adicionar um novo aluno
-router.get("/add",  async (req, res) => {
-  try {
-    const cursos = await Curso.findAll();
+// Formulário para adicionar aluno
+router.get("/add", (req, res) => {
     res.render("base", {
-      title: "Add Aluno",
-      view: "alunos/add",
-      cursos,
+        title: "Add Aluno",
+        view: "alunos/add",
     });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Erro ao recuperar alunos");
-  }
 });
 
-// Adicionar um novo aluno
-router.post("/add",  async (req, res) => {
-  try {
-    const { nome, turma, cursoId } = req.body;
-    await Aluno.create({
-      nome,
-      turma,
-      cursoId,
-    });
+// Adicionar nova aluno
+router.post("/add", async (req, res) => {
+    await Aluno.create({ nome: req.body.nome });
     res.redirect("/alunos");
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Erro ao adicionar Aluno");
-  }
 });
 
-// Formulário para editar um aluno
-router.get("/edit/:id",  async (req, res) => {
-  try {
-    const { id } = req.params;
-    const aluno = await Aluno.findByPk(id, {
-      include: [{ model: Curso, as: "Curso" }],
-    });
-    const cursos = await Curso.findAll();
-    if (aluno) {
-      res.render("base", {
-        title: "Edit Aluno", 
+// Formulário para editar aluno
+router.get("/edit/:id", async (req, res) => {
+    const aluno = await Aluno.findByPk(req.params.id);
+
+    res.render("base", {
+        title: "Editar Aluno",
         view: "alunos/edit",
         aluno,
-        cursos,
-      });
-    } else {
-      res.status(404).send("Aluno não encontrado");
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Erro ao recuperar Aluno");
-  }
+    });
 });
 
-// Atualizar um aluno
-router.post("/edit/:id",  async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { nome, turma, cursoId } = req.body;
-    const aluno = await Aluno.findByPk(id);
-    if (aluno) {
-      await aluno.update({ nome, turma, cursoId });
-      res.redirect("/alunos");
-    } else {
-      res.status(404).send("Aluno não encontrado");
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Erro ao atualizar o Aluno");
-  }
+
+// Atualizar aluno
+router.post("/edit/:id", async (req, res) => {
+    await Aluno.update(
+        { nome: req.body.nome },
+        {where: { id: req.params.id },}
+    );
+    res.redirect("/alunos");
 });
 
-// Deletar um aluno
-router.post("/delete/:id",  async (req, res) => {
-  try {
-    const { id } = req.params;
-    const aluno = await Aluno.findByPk(id);
-    if (aluno) {
-      await aluno.destroy();
-      res.redirect("/alunos");
-    } else {
-      res.status(404).send("Aluno não encontrado");
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Erro ao excluir aluno");
-  }
+// Deletar aluno
+
+router.post("/delete/:id", async (req, res) => {
+    await Aluno.destroy({ where: { id: req.params.id } });
+    res.redirect("/alunos");
 });
 
 module.exports = router;
